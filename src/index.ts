d046260d2b167c9ef8aa077ca929e0c0dabf946d
Ravi Teppala <raviteppala@Ravis-MacBook-Air.local>
2026-01-04 02:22:21 +0530

    New apis added for dashboard and students list

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,7 @@ const app = express();
  * Add new domains here safely
  */
 const ALLOWED_ORIGINS = new Set([
-  "http://localhost:3000",
+  "http://localhost:3000","http://localhost:3002",
   "https://uvv-orcin.vercel.app",
 ]);
 
